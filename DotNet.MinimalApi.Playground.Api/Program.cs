079cb052f43a65cf7477afbaf6f4f26de45051b2
shubham211995 <shubhamtiwari211995@gmail.com>
2026-01-24 22:56:52 +0530

    register global exception middleware

--- DotNet.MinimalApi.Playground.Api/Program.cs
+++ DotNet.MinimalApi.Playground.Api/Program.cs
@@ -1,9 +1,11 @@
+using DotNet.MinimalApi.Playground.Api.Middleware;
 using DotNet.MinimalApi.Playground.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+app.UseMiddleware<GlobalExceptionMiddleware>();
 
 // --------------------
 // Endpoints
