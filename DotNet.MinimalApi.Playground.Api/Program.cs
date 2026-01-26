f093d9a24bf5337b9be54dda0eafe67b4cbc18b7
shubham211995 <shubhamtiwari211995@gmail.com>
2026-01-26 20:38:16 +0530

    add and enable Serilog for structured logging

--- DotNet.MinimalApi.Playground.Api/Program.cs
+++ DotNet.MinimalApi.Playground.Api/Program.cs
@@ -2,15 +2,23 @@ using DotNet.MinimalApi.Playground.Api.Middleware;
 using DotNet.MinimalApi.Playground.Api.Endpoints;
 using DotNet.MinimalApi.Playground.Api.Data;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .WriteTo.Console()
+    .CreateLogger();
+
+builder.Host.UseSerilog();
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlite("Data Source=app.db");
 });
 
 var app = builder.Build();
-
+app.UseSerilogRequestLogging();
 
 
 app.UseHttpsRedirection();
